c7c0c2979958f6d7c43835120efc6e5eacc50f89
rorubyy <roruby118@gmail.com>
2023-05-12 18:37:44 +0800

    can train normally now

--- yolov8/ultralytics/yolo/engine/trainer.py
+++ yolov8/ultralytics/yolo/engine/trainer.py
@@ -332,6 +332,8 @@ class BaseTrainer:
                         else self.loss_items
 
                 # Backward
+                # EDIT
+                torch.use_deterministic_algorithms(False)                
                 self.scaler.scale(self.loss).backward()
 
                 # Optimize - https://pytorch.org/docs/master/notes/amp_examples.html
