3058bcd9e9f2092e5726f8afbeb3d42a2356d23a
rorubyy <roruby118@gmail.com>
2023-06-02 17:33:50 +0800

    add swin transformer block and layer

--- yolov8/ultralytics/nn/tasks.py
+++ yolov8/ultralytics/nn/tasks.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 
 from ultralytics.nn.modules import (C1, C2, C3, C3TR, GPT, SPP, SPPF, Add, Add2, Bottleneck, BottleneckCSP, C2f, C3Ghost, C3x, Classify,
                                     Concat, Conv, ConvTranspose, Detect, DWConv, DWConvTranspose2d, Ensemble, Focus,
-                                    GhostBottleneck, GhostConv, Pose, Segment)
+                                    GhostBottleneck, GhostConv, Pose, Segment, SwinTransformerBlock)
 from ultralytics.yolo.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.yolo.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.yolo.utils.plotting import feature_visualization
@@ -531,6 +531,9 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is GPT:
             c2 = ch[f[0]]
             args = [c2]
+        elif m is SwinTransformerBlock:
+            c2 = ch[f[0]]
+            args = [c1, c2]
         elif m is Add2:
             c2 = ch[f[0]]
             args = [c2, args[1]]
